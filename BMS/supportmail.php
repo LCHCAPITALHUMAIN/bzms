d70806c8f864e3be9ab6ea752bd19e01a49b4d17
Nohard1 <Nohard1@be529d25-1853-0410-bffc-ff9b8461c645>
2008-07-28 19:41:35 +0000

    first time uploading

---
+++ BMS/supportmail.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+include("include/all.php");
+$mains->check($sitemainconf[site_style],$sitemainconf[site_lang]);
+$head=$templates->header($sitemainconf[site_title],$sitemainpath,$_SESSION[site_style],$_SESSION[site_lang]);
+echo $head;
+members::checkpermission(2);
+include("mails.html");
+
+echo $footer;
+?>
